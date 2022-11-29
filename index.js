e586d528243054a27ad5f30062bd3aa827bd3f41
Sumankdatta <sumankdatta18@gmail.com>
2022-11-29 19:40:07 +0600

    first commit added

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+
+app.use(cors())
+app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f1hhq8d.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+ 
+}
+run().catch(console.log)
+
+app.get('/', async (req, res) => {
+  res.send('Hello World!')
+});
+
+
+app.listen(port, () => {
+  console.log(`bike sales running on port ${port}`)
+})
